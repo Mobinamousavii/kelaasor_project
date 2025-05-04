05717c2b1854d2dab31eaa339adec98348b8fe3d
Mobinamousavii <mobinamsv6@gmail.com>
2025-05-04 11:49:18 +0330

    apps registered in settings

--- kelaasor_project1/settings.py
+++ kelaasor_project1/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'advcourses',
+    'blogs',
+    'bootcamps',
+    'payments',
+    'tickets',
 ]
 
 MIDDLEWARE = [
