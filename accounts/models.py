a2dd7c286a0aa54b204ebf88cc3a3ca00298413d
Mobinamousavii <mobinamsv6@gmail.com>
2025-05-06 11:01:50 +0330

    models for the accounts app added and user registered in settings

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
 from django.db import models
+from django.utils import timezone
+import re
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def normalize_phone(self, phone):
+        return re.sub(r'\D', '', phone)
+    
+    def create_user(self, phone, password =None, **extra_fields):
+        if not phone:
+            raise ValueError('phone number is required.')
+        phone = self.normalize_phone(phone)
+        user = self.model(phone = phone, **extra_fields)
+        user.set_password(password)  
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, phone, password = None, **extra_fields):
+        extra_fields.setdefault('is-staff',True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(phone, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    phone = models.CharField(max_length=15, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    data_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone
--- kelaasor_project1/settings.py
+++ kelaasor_project1/settings.py
@@ -147,3 +147,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+
+AUTH_USER_MODEL = 'accounts.User'
