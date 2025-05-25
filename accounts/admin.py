344c96b52770a8f4e64f02cf0dcaee50569f4572
Mobinamousavii <mobinamsv6@gmail.com>
2025-05-25 12:10:07 +0330

    feat(admin): add inline UserProfile

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,24 +2,31 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, UserProfile
 
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'پروفایل'
+    fk_name = 'user'
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    inlines = [UserProfileInline]
     model = User
     list_display = ['phone', 'role', 'is_staff', 'is_active']
     list_filter = ['role', 'is_staff', 'is_active']
     search_fields = ['phone']
     ordering = ['-date_joined']
     
-    fieldsets = (
-        (None, {'fields': ('phone', 'password')}),
-        ('اطلاعات شخصی', {'fields': ('role',)}),
-        ('دسترسی‌ها', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('تاریخ‌ها', {'fields': ('last_login',)}),
-    )
+    #ask how this works 
+    # fieldsets = (
+ 
+    # )
 
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('phone', 'password1', 'password2', 'role', 'is_staff', 'is_active')}
-        ),
-    )
+    # add_fieldsets = (
+
+    # )
+
+    def get_inline_instances(self, request, obj = None):
+        if not obj:
+            return[]
+        return super().get_inline_instances(request, obj)
