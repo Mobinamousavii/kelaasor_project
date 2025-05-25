dce484670560a6eeec1a2f4a474ca6c5652c4b4e
Mobinamousavii <mobinamsv6@gmail.com>
2025-05-25 14:51:16 +0330

    feat(bootcamp): Register models in admin.py and customize list display

--- bootcamps/admin.py
+++ bootcamps/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Bootcamp, BootcampRegistrationRequest, BootcampUser
 
-# Register your models here.
+@admin.register(Bootcamp)
+class BootcampAdmin(admin.ModelAdmin):
+    list_display = ['title', 'start_date', 'status', 'capacity']
+    list_filter = ['status', 'start_date']
+    search_fields = ['title']
+    ordering = ['-start_date']
+
+@admin.register(BootcampRegistrationRequest)
+class BootcampRequestAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'phone', 'bootcamp', 'role', 'status', 'created_at']
+    list_filter = ['status', 'role']
+    search_fields = ['full_name', 'phone']
+    ordering = ['-created_at']
+
+@admin.register(BootcampUser)
+class BootcampUserAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bootcamp', 'role', 'joined_at']
+    list_filter = ['role']
+    search_fields = ['user__phone']
+    ordering = ['-joined_at']
--- kelaasor_project1/urls.py
+++ kelaasor_project1/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/accounts/', include('accounts.urls')),
+    path('api/bootcamps/',include('bootcamps.urls')),
 ]
