742accad159e9824580d0084f5f2be57ef19260c
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-07-23 00:37:39 +0330

    add models table for blogs app

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+
+
+class CategoryBlog(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+    date_created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"Category name is {self.name}"
+    
+
+
+
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    uploaded_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    slug = models.SlugField(unique=True)
+
+
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published")
+    )
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="draft")
+    blogcategory = models.ForeignKey(to=CategoryBlog, on_delete=models.CASCADE, null=True, blank=True)
+    file = models.FileField(upload_to="articles/", null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
